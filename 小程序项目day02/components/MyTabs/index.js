522a50b6926d13f414c77fdfe9410d00960c674f
superxmanyang <460701143@qq.com>
2019-09-23 19:01:11 +0800

    自定义组件的功能实现

--- 小程序项目day02/components/MyTabs/index.js
+++ 小程序项目day02/components/MyTabs/index.js
@@ -1,26 +1,33 @@
 // components/MyTabs/index.js
 Component({
 
- 
+ properties:{
+   titleList:{
+    type:Array,
+    value:[]
+   },
+   currentIndex:{
+     type:Number,
+     value:0
+   },
+ },
 
   data: {
-  titleList:[
-
-    { id: 0, text: "新闻" },
-    { id: 0, text: "新闻" },
-    { id: 0, text: "新闻" },
-  ],
+  
   // 被选中的元素的索引
-  currentIndex:0
+  // currentIndex:0,
   },
-
+  
    methods:{
      handleTap(e){
       //  console.log(e)
       const {index} = e.target.dataset;
-      this.setData({
-        currentIndex:index
-      })
+      // this.setData({
+      //   currentIndex:index
+      // })
+
+
+      this.triggerEvent("itemChange",{index})
      }
    }
 
