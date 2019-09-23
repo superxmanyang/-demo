82855a5dee145c6b96cd99a98b49c8c042b367f8
superxmanyang <460701143@qq.com>
2019-09-23 20:46:17 +0800

    自定义组件的使用

--- 小程序项目day02/pages/slot2/index.js
+++ 小程序项目day02/pages/slot2/index.js
@@ -2,6 +2,18 @@
 Page({
 
   data: {
-    
+    titleList:[
+
+      { id: 0, text: "要闻" },
+      { id: 1, text: "推荐" },
+      { id: 2, text: "原创" },
+    ],
+    index:0
+  },
+  handleItemChange(e){
+    // console.log(e);
+    this.setData({
+      index:e.detail.index
+    })
   }
 })
